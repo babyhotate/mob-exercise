8e6e7d1075b6e6bd2bbf08e73eabdeb516fa889f
aksh-t <tanno.ao@gmail.com>
2020-04-25 15:50:57 +0900

    Co-authored-by: inukawaii Co-authored-by: nakanoh Co-authored-by: shellzu

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,5 +4,27 @@
 // ルール3. 15の倍数の時は、"fizz-buzz" と出力する
 // ルール4. 上記以外の数字の場合は、そのまま数字を出力する
 
-console.log("test");
-console.log('hoge');
+for (let index = 1; index <= 100; index++) {
+    let text = index;
+
+    if (index % 15 == 0){
+        text = 'fizz-buzz'
+    } else if ((index % 3) == 0) {
+        text = 'fizz';
+    } else if (index % 5 == 0){
+        text = 'buzz'
+    } 
+    console.log(text);
+}
+
+// for (let index = 1; index <= 100; index++) {
+//     if (index % 15 == 0){
+//         console.log("fizz-buzz");
+//     } else if ((index % 3) == 0) {
+//         console.log("fizz");
+//     } else if (index % 5 == 0){
+//         console.log("buzz");
+//     } else {
+//         console.log(index);
+//     }
+// }
