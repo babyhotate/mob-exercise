52da1da86deeb5e471e581e133f8c8e6ab8c3ba0
aksh-t <tanno.ao@gmail.com>
2020-04-25 17:21:57 +0900

    15で正しく計算できた状態

--- fizzBuzzSum.js
+++ fizzBuzzSum.js
@@ -2,7 +2,10 @@
 // ルール1. 3の倍数の時は、"fizz" 
 // ルール2. 5の倍数の時は、"buzz" 
 // ルール3. 15の倍数の時は、"fizz-buzz" 
-// ルール4. 上記以外の数字の場合は、そのまま
+// ルール4. 上記以外の数字の場合は、えそのまま
+
+const result = [];
+let resultSum =0;
 
 for (let index = 1; index <= 15; index++) {
     let text = index;
@@ -13,7 +16,17 @@ for (let index = 1; index <= 15; index++) {
         text = 'fizz';
     } else if (index % 5 == 0){
         text = 'buzz'
-    } 
-    console.log(text);
+    } else {
+        result.push(index);
+    }
 }
-// expect: 60
+
+// console.log(result);
+
+for (let index = 0; index < result.length; index++) {
+    resultSum += result[index];
+    // console.log(resultSum);
+}
+console.log(resultSum);
+//  15 -> expect: 60
+//  1000000 -> expect: 266666333332
